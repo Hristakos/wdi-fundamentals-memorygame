da980596a29236f549e858b1bf8be8a6447f1f89
Peter Hristakos <peterhristakos@Peters-MacBook-Pro.local>
2020-02-29 17:41:58 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+
+
+let cardOne = "king";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
